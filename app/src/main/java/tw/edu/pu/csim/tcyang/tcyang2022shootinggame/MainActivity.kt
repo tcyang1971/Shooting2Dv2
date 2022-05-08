929cd84bcefdc41d126dd768ca32e63057fe6d0c
tcyang1971 <tcyang@gm.pu.edu.tw>
2022-05-08 16:58:34 +0800

    v.3 Glide與Chain

--- app/src/main/java/tw/edu/pu/csim/tcyang/tcyang2022shootinggame/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/tcyang2022shootinggame/MainActivity.kt
@@ -1,11 +1,14 @@
 package tw.edu.pu.csim.tcyang.tcyang2022shootinggame
 
-import android.graphics.BitmapFactory
 import android.graphics.Canvas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
 import kotlinx.coroutines.*
+@GlideModule
+public final class MyAppGlideModule : AppGlideModule()
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var game : Game
     var flag:Boolean = false
     lateinit var job : Job
+    lateinit var imgAuthor : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +24,14 @@ class MainActivity : AppCompatActivity() {
 
         img = findViewById(R.id.img)
         game = findViewById(R.id.game)
+
+        imgAuthor = findViewById(R.id.imgAuthor)
+        GlideApp.with(this)
+            //.load(R.drawable.earth)
+            .load(R.drawable.tcyang)
+            .circleCrop()
+            .override(800, 600)
+            .into(imgAuthor)
 
         img.setOnClickListener({
             if (flag){
